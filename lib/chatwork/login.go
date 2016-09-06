cc2d187e53ea8d17618f245ebdafdc1fa3f8182a
UshioShugo <ushio.s@gmail.com>
2016-09-06 18:54:41 +0900

    got access token

--- lib/chatwork/login.go
+++ lib/chatwork/login.go
@@ -1,10 +1,12 @@
-package cw
+package chatwork
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
-	"strings"
+	"regexp"
 )
 
 const (
@@ -12,28 +14,52 @@ const (
 	BaseURL = "https://www.chatwork.com"
 )
 
-// Login to chatwork
-func Login(email, pass string) (string, error) {
+var (
+	AccessTokenRegExp *regexp.Regexp
+)
+
+func init() {
+	AccessTokenRegExp = regexp.MustCompile(`var ACCESS_TOKEN = '(.+)'`)
+}
+
+// Login to chatwork and return ACCESSTOKEN
+func Login(email, pass string) ([]byte, error) {
 	values := url.Values{}
 	values.Add("email", email)
 	values.Add("password", pass)
 	values.Add("autologin", "on")
 
-	client := &http.Client{}
-	resp, err := client.Post(
-		chatworkURL("/login.php"),
-		"application/x-www-form-urlencoded",
-		strings.NewReader(values.Encode()),
-	)
+	jar, err := cookiejar.New(nil)
 	if err != nil {
-		return "", err
+		return []byte{}, err
+	}
+	client := &http.Client{
+		Jar: jar,
+	}
+	_, err = client.PostForm(u("/login.php"), values)
+	if err != nil {
+		return []byte{}, err
 	}
 
-	fmt.Println(resp)
+	resp, err := client.Get(u("/"))
+	if err != nil {
+		return []byte{}, err
+	}
+	defer resp.Body.Close()
 
-	return "", nil
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	regRes := AccessTokenRegExp.FindSubmatch(body)
+	if len(regRes) < 2 {
+		return []byte{}, fmt.Errorf("cannot found ACCESS_TOKEN in %s ", u("/"))
+	}
+
+	return regRes[1], nil
 }
 
-func chatworkURL(path string) string {
+func u(path string) string {
 	return fmt.Sprintf("%s%s", BaseURL, path)
 }
--- lib/chatwork/login_test.go
+++ lib/chatwork/login_test.go
@@ -1,1 +1,24 @@
-package cw
+package chatwork
+
+import (
+	"fmt"
+	"testing"
+)
+
+var (
+	email = ""
+	pass  = ""
+)
+
+func TestLogin(t *testing.T) {
+	test := func(id, pass string) {
+		res, err := Login(id, pass)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fmt.Println(string(res))
+	}
+
+	test(email, pass)
+}
